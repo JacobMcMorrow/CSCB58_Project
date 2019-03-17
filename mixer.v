856b946bf8ec86f1677cd2341f6cef0f86f125db
mcmorro4 <jacob.mcmorrow@mail.utoronto.ca>
2019-03-16 23:17:04 -0400

    Finished mixed module. Added shell mixer modules.

--- mixer.v
+++ mixer.v
@@ -16,18 +16,18 @@ module mixer(mix_down
 	wire [17:0] add_one0, add_one1;
 	wire [18:0] add_two;
 
-	// First layer
-	adder_zero a0(.out(add_zero0), .clk(CLOCK_50));
-	adder_zero a1(.out(add_zero1), .clk(CLOCK_50));
-	adder_zero a2(.out(add_zero2), .clk(CLOCK_50));
-	adder_zero a3(.out(add_zero3), .clk(CLOCK_50));
+	// First stage
+	adder_zero a0(.out(add_zero0), .in0(audio0), .in1(audio1), .clk(CLOCK_50));
+	adder_zero a1(.out(add_zero1), .in0(audio2), .in1(audio3), .clk(CLOCK_50));
+	adder_zero a2(.out(add_zero2), .in0(audio4), .in1(audio5), .clk(CLOCK_50));
+	adder_zero a3(.out(add_zero3), .in0(audio6), .in1(audio7), .clk(CLOCK_50));
 	
-	// Second layer
-	adder_one a4(.out(add_one0), .clk(CLOCK_50));
-	adder_one a5(.out(add_one1), .clk(CLOCK_50));
+	// Second stage
+	adder_one a4(.out(add_one0), .in0(add_zero0), .in1(add_zero1), .clk(CLOCK_50));
+	adder_one a5(.out(add_one1), .in0(add_zero2), .in1(add_zero3), .clk(CLOCK_50));
 
-	// Third layer
-	adder_two a6(.out(add_two), .clk(CLOCK_50));
+	// Third stage
+	adder_two a6(.out(add_two), .in0(add_one0), .in1(add_one1), .clk(CLOCK_50));
 
 	assign mix_down = add_two;
 
