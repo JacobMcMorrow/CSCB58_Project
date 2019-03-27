1042ec2178f326ae561cc0ae188d4aec5eeeb351
mcmorro4 <jacob.mcmorrow@mail.utoronto.ca>
2019-03-27 13:40:32 -0400

    Updated synth_snare, wave, and mixer to match new sine wave strategy.

--- DoNotOpen/synth_snare.v
+++ DoNotOpen/synth_snare.v
@@ -1,37 +1,80 @@
-module synth_snare();
+module synth_snare(out, reset, clk);
+	output [] out;
+	input reset, clk;
+
+	wire [11:0] pulse_180_out, pulse_330_out;
+	wire sine_sync, tri_sync, pulse_180_msb, pulse_330_msb;
 
 	// Oscilator section
 	// Sine 180Hz
-	wave sine180(
-		.tone_freq(),
+	wave pulse180(
+		.tone_freq(16'b0),
 		.pulse_width(),
-		.main_clk(),
+		.main_clk(clk),
 		.sample_clk(),
-		.reset(),
+		.reset(reset),
 		.test(),
-		.en_noise(),
-		,en_triangle(),
-		.en_sine()
+		.out(pulse_180_out),
+		.accumulator_msb(pulse_180_msb),
+		.sync_trigger_out(sine_sync),
+		.en_noise(1'b0),
+		.en_pulse(1'b1),
+		,en_triangle(1'b0)
 		);
 
 	// Sine 330Hz
-	wave sine330(
-		.tone_freq(),
+	wave pulse330(
+		.tone_freq(16'b0),
 		.pulse_width(),
-		.main_clk(),
+		.main_clk(clk),
 		.sample_clk(),
-		.reset(),
+		.reset(reset),
 		.test(),
-		.en_noise(),
-		,en_triangle(),
-		.en_sine()
+		.out(pulse_330_out),
+		.accumulator_msb(),
+		.sync_trigget_out(),
+		.en_noise(1'b0),
+		.en_pulse(1'b1),
+		,en_triangle(1'b0)
 		);
 
 	// Sine mixer
+	synth_mixer sine_mixer(clk(clk), .a(), .b());
 
 	// Triangle 111Hz
+wave tri_shifted(
+		.tone_freq(16'b0),
+		.pulse_width(),
+		.main_clk(clk),
+		.sample_clk(),
+		.reset(reset),
+		.test(),
+		.out(),
+		.accumulator_msb(),
+		.sync_trigget_out(),
+		.en_noise(1'b0),
+		.en_pulse(1'b1),
+		,en_triangle(1'b1)
+	);
 
-	// Freq shift triangle 
+	wave tri_shifted_(
+		.tone_freq(16'b0),
+		.pulse_width(),
+		.main_clk(clk),
+		.sample_clk(),
+		.reset(reset),
+		.test(),
+		.out(),
+		.accumulator_msb(),
+		.sync_trigget_out(),
+		.en_noise(1'b0),
+		.en_pulse(1'b0),
+		,en_triangle(1'b1)
+	);
+
+	// Freq shift triangle
+	synth_mixer tri_mixer(clk(clk), .a(), .b());
+
 
 	// Triangle mixer
 
