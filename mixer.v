cd7bbbe029121c4e55df4deaa4b28975c43caa5f
chrling <chr.ling@alumni.utoronto.ca>
2019-03-29 22:52:21 -0400

    removed testing outputs

--- mixer.v
+++ mixer.v
@@ -1,8 +1,4 @@
 module mixer(mix_down,
-					audio0out, // TESTINg
-					audio1out, // T
-					audio2out, // T
-					audio3out, // T
 						 audio0,
 						 audio1,
 						 audio2,
@@ -11,16 +7,10 @@ module mixer(mix_down,
 						 );
 	input [7:0] audio0, audio1, audio2, audio3;
 	input clk;
-	output [7:0] audio0out, audio1out, audio2out, audio3out;
 	output [31:0] mix_down;
 
 	wire [8:0] add_zero0, add_zero1;
 	wire [9:0] add_one;
-	
-	assign audio0out = audio0;
-	assign audio1out = audio1;
-	assign audio2out = audio2;
-	assign audio3out = audio3;
 
 	// First stage
 	adder_zero a0(.out(add_zero0), .in0(audio0), .in1(audio1), .clk(clk));
