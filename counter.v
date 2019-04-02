8fe7ca6e0742e2169d83bc578107a44159c56315
mcmorro4 <jacob.mcmorrow@mail.utoronto.ca>
2019-04-02 08:02:37 -0400

    Added commenting to counter.

--- counter.v
+++ counter.v
@@ -1,20 +1,31 @@
 module counter(count, done, clk, slow_clk, ins_signal);
+	// 13-bit number corresponding to a ROM address
 	output reg [12:0] count;
+	// Signal to indicate counting finished
 	output reg done;
+	// 50MHz clock, bpm clock, and signal indicating instrument is playing
 	input  clk, slow_clk, ins_signal;
 	
 	// counter state machine
 	reg current_count_state, next_count_state, slow_reg;
+	// MAXCOUNT is the last address in each sample
 	localparam counting = 1'b1, waiting = 1'b0, MAXCOUNT = 13'd8191; 
-		
+
+	// Set slow register to 0 while counting
 	always @(*) begin
 	if (slow_clk)
 		slow_reg <= 1'b1;
 	else if (current_count_state == counting)
 		slow_reg <= 1'b0;
 	end
-	
-		
+
+	/*
+		 While in the waiting state, if we have received a slow clock signal, start
+		 counting. Otherwise continue waiting
+
+		 If we are in the counting state and reach MAXCOUNT, set the next state to
+		 waiting, otherwise continue counting
+	*/
 	always @(*)
 	begin: counter_state_table
 		case(current_count_state)
@@ -23,7 +34,8 @@ module counter(count, done, clk, slow_clk, ins_signal);
 			default: next_count_state = waiting;
 		endcase
 	end
-	
+
+	// Set state to wait while there is no instrument playing
 	always @(posedge clk)
 	begin
 		if (!ins_signal)
@@ -31,7 +43,16 @@ module counter(count, done, clk, slow_clk, ins_signal);
 		else
 			current_count_state <= next_count_state;
 	end
-	
+
+	/*
+		 If we are currently in the waiting state, ensure we have a count of zero
+		 and set done to 1 to signal counting is complete
+
+		 If we are counting there are two possible conditions, either we reached the
+		 maximum count, or have not. In the case we have counted to MAXCOUNT, we set
+		 done to 1 and stop counting. Otherwise, ensure we do not send an
+		 affirmative done signal, set done to 0 and increment count
+	*/
 	always @(posedge clk)
 	begin
 		if (current_count_state == waiting) begin
