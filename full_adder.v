1e72cd0dc94d3380b3301a2b1dd6c12fada180fd
mcmorro4 <jacob.mcmorrow@mail.utoronto.ca>
2019-04-03 12:03:19 -0400

    Apparently I changed something in  full adder, refer back here if something broke for no reason.

--- full_adder.v
+++ full_adder.v
@@ -1,7 +1,7 @@
 module full_adder(cout, sum, a, b, cin, clk);
 	input a, b, cin, clk;
 	output reg cout, sum;
-	
+
 	always @(posedge clk) begin
 		cout = a & b | a & cin | b & cin;
 		sum = a ^ b ^ cin;
